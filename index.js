9ec1d797e1da8c20328aed08d0d0e6e48796baa1
whyshouldibother <prajwalshrestha2018@gmail.com>
2024-02-09 16:26:51 +0545

    qr_scanner

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function html_runner(){
-    document.write(document.getElementById('code').value)
+function html_runner() {
+  document.write(document.getElementById("code").value);
+}
+function scan(){
+    document.getElementById('my-qr-reader').style.display="block";
 }
